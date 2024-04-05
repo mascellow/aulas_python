19d19e3da72ca23c5d7fd90d11ab052f2cc01d22
mascellow <159308467+mascellow@users.noreply.github.com>
2024-04-05 18:29:17 +0000

    segunda aula

---
+++ solicitandodados.py
@@ -0,0 +1,24 @@
+import os
+
+os.system('cls || clear')
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
+op = True
+os.system('cls || clear')
+
+while op:
+    decisao = input("Deseja imprimir os dados?: (S/N)")
+    decisao = decisao.upper()
+    if decisao == 'S':
+        os.system('cls || clear')
+        print(f"Nome: {nome}")
+        print(f"Idade: {idade}")
+        print(f"Altura: {altura}")
+        print(f"peso: {peso}")
+    elif decisao == 'N':
+        op = False
+    else:
+        print("Decisão inválida.")
--- tiposprimitivos.py
+++ tiposprimitivos.py
@@ -3,3 +3,11 @@ idade = 25
 
 print(f"Nome: {nome}")
 print(f"Idade: {idade}")
+
+"""
+int = 1
+float = 2.2
+bool = True
+string = "Marcelo"
+"""
+
