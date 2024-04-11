f6091e4c7d5bc76882fa974fdfc04e5f0cce8e50
mascellow <159308467+mascellow@users.noreply.github.com>
2024-04-11 17:45:41 +0000

    atividade

--- atividade_tabuada.py
+++ atividade_tabuada.py
@@ -2,10 +2,7 @@ import os
 os.system("cls || clear")
 
 numero = int(input("Digite um número para saber a tabuada: "))
-iterador = 0
 soma = 0
 for i in range(11):
-
-    soma = numero * iterador
-    print(f"{numero} X {iterador} = {soma}")
-    iterador += 1
+    soma = numero * i
+    print(f"{numero} X {i} = {soma}")
