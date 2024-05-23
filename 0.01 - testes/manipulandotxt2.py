ae267984394876e94af7080811891dbf4373e16e
mascellow <159308467+mascellow@users.noreply.github.com>
2024-05-23 16:55:44 +0000

    Commit Inicial

--- 0.01 - testes/manipulandotxt2.py
+++ 0.01 - testes/manipulandotxt2.py
@@ -1,20 +1,27 @@
 import os
-class Livro:
+class Livros:
     def __init__(self, nome, autor, categoria, preco):
         self.nome = nome
         self.autor = autor
         self.categoria = categoria
         self.preco = preco
-os.system("cls || clear")
+
+def salvar(livros):
+    with open("Catálogo_Livros.txt", "w", encoding="utf-8")as arquivo:
+        arquivo.write("=== Livros Cadastrados ===")
+        for livro in livros:
+            arquivo.write(f"Nome: {livro.nome}, Autor: {livro.autor}, Categoria: {livro.categoria}, Preço: {livro.preco}")
 livros = []
-for i in range(3):
-    nome = input("Nome do livro: ")
-    autor = input("Autor: ")
-    categoria = input("Categoria: ")
-    preco = float(input("Preço: "))
-    livro = Livro(nome,autor,categoria,preco)
+
+for i in range(1):
+    os.system("cls || clear")
+    print("=== Cadastro de livros ===\n")
+    nome = input("Digite o nome do livro: ")
+    autor = input("Digite o nome do autor: ")
+    categoria = input("Digite a categoria: ")
+    preco = float(input("Digite o preço: "))
+    livro = Livros(nome,autor,categoria,preco)    
     livros.append(livro)
 
-with open("Catálogo_Livros.txt", "a") as arquivo:
-    for livro in livros:
-        arquivo.write(f"Nome: {livro.nome},Autor: {livro.autor},Categoria: {livro.categoria},Preço: {livro.preco}\n")
+
+salvar(livros)
